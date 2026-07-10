60bc6555afe48397992265a88433391d55925038
FarzanehTaravati <farzaneh.taravati.dev@gmail.com>
2026-07-10 14:31:07 +0200

    feat: implement api

--- src/components/Work-list/work-list.jsx
+++ src/components/Work-list/work-list.jsx
@@ -2,24 +2,42 @@ import { Check, List } from 'lucide-react';
 import { list } from '../mock/work-list';
 import { Trash } from 'lucide-react';
 import "./work-list.css";
+import { useEffect, useState } from 'react';
+import { BASE_URL } from '../../server/constants';
+const getData = async ()=>{
+   const data = await fetch(`${BASE_URL}/toDos`);
+   const res = await data.json();
+   return res
+}
 
 export const Worklist = (props) => {
+
+    const [toDos, setToDOs] = useState([]);
+
+    useEffect(() => {
+    getData().then((res)=>{setToDOs(res)})
+        // console.log(data);
+        // setToDOs(data)
+    },[])
+    
+
+
     return (
         <div className='workList'>
 
-            <span><List /><p> لیست کارها</p></span>
+            <span className='workList-title'><List /><p> لیست کارها</p></span>
 
-            {list.map((work) => {
+            {toDos.map((work) => {
                 return (
                     <div className='work-cart-container'>
                         {/* <p>{work.id}</p> */}
-                        <input type='checkbox'></input>
+                        <input className='work-cart-check' type='checkbox'></input>
                         <div className='work-cart' >
                             <div className='work-cart-text' >
-                                <p>{work.title}</p>
+                                <p className='work-cart-title'>{work.title}</p>
                                 <p>{work.status === "done" ? <p className="status-done status">انجام شده</p> : <p className='status-doing status'>در حال انجام</p>}</p>
                             </div>
-                            <Trash />
+                            <span className='cart-trashIcon'><Trash style={{ color: "red" }} /></span>
                         </div>
                     </div>
                 )
---
+++ src/server/constants.js
@@ -0,0 +1,2 @@
+export  const BASE_URL = "https://6a50d2bdc576c846dcb9e77c.mockapi.io/api"
+
