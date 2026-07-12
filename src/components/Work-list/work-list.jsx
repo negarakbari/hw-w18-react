ab6a4e9d861cd82a2ffbb3888a7aea398778a55a
amirshafie <amirshafie738@gmail.comgit config --global user.name amirshafiegit config --global user.email amirshafie738@gmail.com>
2026-07-12 19:18:56 +0330

    feat:create delete handler

--- src/components/Work-list/work-list.jsx
+++ src/components/Work-list/work-list.jsx
@@ -1,49 +1,73 @@
-import { List } from 'lucide-react';
-import { Trash } from 'lucide-react';
+import { List, Torus } from "lucide-react";
+import { Trash } from "lucide-react";
 import "./work-list.css";
-import { useEffect, useState } from 'react';
-import { BASE_URL } from '../../server/constants';
+import { useEffect, useState } from "react";
+import { BASE_URL } from "../../server/constants";
 
 const getData = async () => {
-    const data = await fetch(`${BASE_URL}/todo`);
-    const res = await data.json();
-    return res
-}
-
+  const data = await fetch(`${BASE_URL}/todo`);
+  const res = await data.json();
+  return res;
+};
+const deleteData = async (id) => {
+  const data = await fetch(`${BASE_URL}/todo/${id}`, {
+    method: "DELETE",
+  });
+};
 export const Worklist = (props) => {
+  const [toDos, setToDOs] = useState([]);
+const [loading,setLoading]= useState(false);
 
-    const [toDos, setToDOs] = useState([]);
+  useEffect(() => {
+    dataHandler();
+  }, [props.isSubmitting]);
 
-    useEffect(() => {
-        dataHandler()
-    }, [props.isSubmitting])
+  const dataHandler = async () => {
+    setLoading(true)
+    getData().then((res) => {
+      setToDOs(res);
+    }).finally(()=>setLoading(false));
+  };
 
-    const dataHandler = async () => {
-        getData().then((res) => { setToDOs(res) })
-    }
-    
+  const deleteHandler = (id) => {
+    deleteData(id).then(()=> dataHandler());
+  };
 
+  return (
+    <div className="workList">
+      <span className="workList-title">
+        <List />
+        <p> لیست کارها</p>
+      </span>
 
-    return (
-        <div className='workList'>
-
-            <span className='workList-title'><List /><p> لیست کارها</p></span>
-
-            {toDos?.map((work) => {
-                return (
-                    <div className='work-cart-container'>
-                        {/* <p>{work.id}</p> */}
-                        <input className='work-cart-check' type='checkbox'></input>
-                        <div className='work-cart' >
-                            <div className='work-cart-text' >
-                                <p className='work-cart-title'>{work.title}</p>
-                                <p>{work.status ? <p className="status-done status">انجام شده</p> : <p className='status-doing status'>در حال انجام</p>}</p>
-                            </div>
-                            <span className='cart-trashIcon'><Trash style={{ color: "red" }} /></span>
-                        </div>
-                    </div>
-                )
-            })}
-        </div>
-    )
-}
+      {loading? <span>loading </span>: toDos?.map((work) => {
+        return (
+          <div key={work.id} className="work-cart-container">
+            {/* <p>{work.id}</p> */}
+            <input className="work-cart-check" type="checkbox"></input>
+            <div className="work-cart">
+              <div className="work-cart-text">
+                <p className="work-cart-title">{work.title}</p>
+                <p>
+                  {work.status ? (
+                    <p className="status-done status">انجام شده</p>
+                  ) : (
+                    <p className="status-doing status">در حال انجام</p>
+                  )}
+                </p>
+              </div>
+              <span
+                onClick={() => {
+                  deleteHandler(work.id);
+                }}
+                className="cart-trashIcon"
+              >
+                <Trash style={{ color: "red" }} />
+              </span>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
